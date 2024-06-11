d140187f4cb2c556c96f1639a3b2dbbf276d988e
Rachetai <160573109+Rachetai@users.noreply.github.com>
2024-06-11 07:07:28 +0700

    Add files via upload

---
+++ YIOADGE/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YIOADGE
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("AAAAA - ERROR");
+            Console.ReadLine();
+            // /*  */
+        }
+    }
+}
